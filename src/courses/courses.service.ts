dfe40b548a15d4c6a4dfbf27d4f8cf766938f73b
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-12-22 03:48:32 -0300

    Adding data validation

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -23,6 +23,7 @@ export class CoursesService {
   }
   create(createCourseDTO: any) {
     this.courses.push(createCourseDTO);
+    return createCourseDTO;
   }
   update(id: number, updateCourseDTO: any) {
     const existingCourse = this.findOne(id);

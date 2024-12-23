319c1aeb2bc13872c76dd838ef2d9ae2af392388
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-12-23 00:04:16 -0300

    Adding relations

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,11 +3,12 @@ import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/courses.entity';
+import { Tag } from './entities/tags.entity';
 
 @Module({
   //Precisa importar o typeorm nos módulos que forem criadas entidades:
   //indicando as entidades a serem manipuladas:
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Tag])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })

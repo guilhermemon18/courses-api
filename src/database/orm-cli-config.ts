7c771146ebc091f272df32183cd6e9d46c5a5798
guilhermemon18 <guilhermemonsani7@gmail.com>
2024-12-24 05:23:24 -0300

    Creating migration AddTagsIdToCoursesTagsTAble

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,18 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1734999591459 } from 'src/migrations/1734999591459-CreateCoursesTable';
 import { CreateTagsTable1735001638568 } from 'src/migrations/1735001638568-CreateTagsTable';
+import { CreateCoursesTagsTable1735007886636 } from 'src/migrations/1735007886636-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1735016667012 } from 'src/migrations/1735016667012-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1735020091856 } from 'src/migrations/1735020091856-AddTagsIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1734999591459, CreateTagsTable1735001638568],
+  migrations: [
+    CreateCoursesTable1734999591459,
+    CreateTagsTable1735001638568,
+    CreateCoursesTagsTable1735007886636,
+    AddCoursesIdToCoursesTagsTable1735016667012,
+    AddTagsIdToCoursesTagsTable1735020091856,
+  ],
 });
